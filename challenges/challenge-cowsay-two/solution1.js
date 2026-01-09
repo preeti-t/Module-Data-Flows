45b0037317933a51896a07d25f5fa6ca641601d2
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-01-09 11:14:08 +0100

    Update solution1.js
    answered all questions

--- challenges/challenge-cowsay-two/solution1.js
+++ challenges/challenge-cowsay-two/solution1.js
@@ -7,24 +7,51 @@
 // 1. Accept arguments
 
 // how will you accept arguments?
+const args = process.argv.slice(2);
+let saying = args.join(" ");
 
 // 2. Make supplies for our speech bubble
 
 let topLine = '_';
 let bottomLine = '-';
-let saying = '';
+let sayingDefault = 'Mooooo';
 
 // 3. Make a cow that takes a string
 
 function cowsay(saying) {
-// how will you make the speech bubble contain the text?
+
+
+  // how will you make the speech bubble contain the text?
+  // → Repeat characters based on the length of the string
+  topLine = "_".repeat(saying.length + 2);
+  bottomLine = "-".repeat(saying.length + 2);
+
 
 // where will the cow picture go?
+  // → Returned as part of the template string below
+  return `
+ ${topLine}
+< ${saying} >
+ ${bottomLine}
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+`;
+}
+
+
 
 // how will you account for the parameter being empty?
-
-}
+// how will you make the speech bubble contain the text?
+   // → If no argument is provided, use a default saying
+  if (!saying) {
+    saying = sayingDefault;
+  }
 
 //4. Pipe argument into cowsay function and return a cow
 
 // how will you log this to the console?
+// → By passing the argument into cowsay() and using console.log
+console.log(cowsay(saying));

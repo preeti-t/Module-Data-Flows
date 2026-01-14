da353985a2175345b1c9c07cceaabd268f6fa484
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-01-14 11:41:17 +0100

    Update convert-to-old-roman.js
    updated as per requirements

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,23 @@
-function convertToOldRoman(n) {}
+function convertToOldRoman(n) {
+  const numerals = [
+    { value: 1000, symbol: "M" },
+    { value: 500, symbol: "D" },
+    { value: 100, symbol: "C" },
+    { value: 50, symbol: "L" },
+    { value: 10, symbol: "X" },
+    { value: 5, symbol: "V" },
+    { value: 1, symbol: "I" }
+  ];
+
+  let result = "";
+
+  for (const numeral of numerals) {
+    const count = Math.floor(n / numeral.value);
+    result += numeral.symbol.repeat(count);
+    n = n % numeral.value;
+  }
+
+  return result;
+}
 
 module.exports = convertToOldRoman;
